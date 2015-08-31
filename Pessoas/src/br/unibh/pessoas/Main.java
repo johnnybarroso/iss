ff0a2d1ca99e5bded2cd323dd56a86358bc723a8
Johnny <Johnny@10.13.10.166>
2015-08-31 19:47:47 -0300

    Enviando dados

---
+++ Pessoas/src/br/unibh/pessoas/Main.java
@@ -0,0 +1,20 @@
+package br.unibh.pessoas;
+
+public class Main {
+
+	public static void main(String args[]) {
+		
+		Pessoa p1 = new Pessoa();
+		p1.setNome("João");
+		
+		Pessoa p2 = new Pessoa(1L, "Maria");
+		
+		Pessoa p3 = new Pessoa(2L, "Tiago", "Rua A", "33333-2222");
+		
+		System.out.println(p1);
+		System.out.println(p2);
+		System.out.println(p3);
+
+	}
+
+}
