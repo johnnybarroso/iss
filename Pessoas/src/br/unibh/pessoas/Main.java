67d946bd881d18293a0b0e2c79e736a4bbfe7e72
Johnny <Johnny@10.13.10.166>
2015-09-01 22:03:50 -0300

    Primeira parte finalizado

--- Pessoas/src/br/unibh/pessoas/Main.java
+++ Pessoas/src/br/unibh/pessoas/Main.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import br.unibh.pessoas.entidades.Pessoa;
 import br.unibh.pessoas.entidades.PessoaFisica;
+import br.unibh.pessoas.entidades.PessoaJuridica;
 
 public class Main {
 
@@ -11,7 +12,10 @@ public class Main {
 		
 		PessoaFisica pf1 = new PessoaFisica(1L, "João", "Rua A", "3333-4444", "11234563121", "joao@gmail.com", new Date(), "M");
 		
+		PessoaJuridica pj1 = new PessoaJuridica(2L,"Realizações LTDA", "Rua B", "3344-5555", "121488744", new Date(), "www.tiberawar.com");
+		
 		System.out.println(pf1);
+		System.out.println(pj1);
 
 	}
 
