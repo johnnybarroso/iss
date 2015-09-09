b3ef48fdd3179570ca8b9f18dd17f8131772c8be
Johnny <Johnny@10.206.37.224>
2015-09-08 21:13:59 -0300

    Atualização

--- Pessoas/src/br/unibh/pessoas/Main.java
+++ Pessoas/src/br/unibh/pessoas/Main.java
@@ -10,7 +10,6 @@ public class Main {
 
 	public static void main(String args[]) {
 		
-		Pessoa P1 = new Pessoa();
 		PessoaFisica pf1 = new PessoaFisica(1L, "João", "Rua A", "3333-4444", "11234563121", "joao@gmail.com", new Date(), "M");
 		PessoaJuridica pj1 = new PessoaJuridica(2L,"Realizações LTDA", "Rua B", "3344-5555", "121488744", new Date(), "www.tiberawar.com");
 		
