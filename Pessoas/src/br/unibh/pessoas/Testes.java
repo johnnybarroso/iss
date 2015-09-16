f5957d776db5fd5ea0c776de81204448e1e5aee9
Johnny <Johnny@10.206.37.224>
2015-09-15 22:35:13 -0300

    Aula de terça 15/09/2015

---
+++ Pessoas/src/br/unibh/pessoas/Testes.java
@@ -0,0 +1,26 @@
+package br.unibh.pessoas;
+
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.unibh.pessoas.entidades.PessoaFisica;
+import br.unibh.pessoas.persistencia.PessoaFisicaDAO;
+
+public class Testes {
+	@Test
+	public void testePessoaFisicaFindAll(){
+		PessoaFisicaDAO dao = new PessoaFisicaDAO();
+		List<PessoaFisica> l = dao.findAll();
+		Assert.assertEquals(l.size(), 100);
+		
+	}
+	@Test
+	public void testePessoaFisicaFind(){
+		PessoaFisicaDAO dao = new PessoaFisicaDAO();
+		PessoaFisica p = dao.find(6L);
+		Assert.assertEquals(p.getNome(), "Jeremy Z. Christian");
+	}
+}
--- Pessoas/src/br/unibh/pessoas/persistencia/PessoaFisicaDAO.java
+++ Pessoas/src/br/unibh/pessoas/persistencia/PessoaFisicaDAO.java
@@ -1,22 +1,68 @@
 package br.unibh.pessoas.persistencia;
 
 import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
+
+import com.mysql.jdbc.PreparedStatement;
 
 import br.unibh.pessoas.entidades.PessoaFisica;
 
 public class PessoaFisicaDAO implements DAO<PessoaFisica, Long>{
-
+	private SimpleDateFormat df = new SimpleDateFormat("yyyy-mm-dd");
 	@Override
 	public PessoaFisica find(Long id) {
-		// TODO Auto-generated method stub
+		try {
+			PreparedStatement p = (PreparedStatement) JDBCUtil.getConnection().prepareStatement(
+					  "select * from tb_pessoa_fisica "
+					+ "where id = ?");
+			p.setLong(1, id);
+			ResultSet res = p.executeQuery();
+			if (res.next()){
+				return new PessoaFisica(
+						res.getLong("id"),
+						res.getString("nome"),
+						res.getString("endereco"),
+						res.getString("telefone"),
+						res.getString("cpf"),
+						res.getString("email"),
+						res.getDate("data_nascimento"),
+						res.getString("sexo")
+						);
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.closeConnection();
+		}
 		return null;
 	}
 
 	@Override
 	public void insert(PessoaFisica t) {
 		// TODO Auto-generated method stub
-		
+		try {
+			PreparedStatement p = (PreparedStatement) JDBCUtil.getConnection().prepareStatement(
+					  "insert into tb_pessoa_fisica "
+					+ "(nome, endereco, telefone, cpf, email, "
+				    + "data_nascimento, sexo) values "
+					+ "(?,?,?,?,?,?,?)");
+			p.setString(1, t.getNome());
+			p.setString(2, t.getEndereco());
+			p.setString(3, t.getTelefone());
+			p.setString(4, t.getCpf());
+			p.setString(5, t.getEmail());
+			p.setString(6, df.format(t.getDataNascimento()));
+			p.setString(7, t.getSexo());
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.closeConnection();
+		}
+		//return null;		
 	}
 
 	@Override
@@ -33,19 +79,31 @@ public class PessoaFisicaDAO implements DAO<PessoaFisica, Long>{
 
 	@Override
 	public List<PessoaFisica> findAll() {
+		List<PessoaFisica> lista = new ArrayList<PessoaFisica>();
 		try {
 			ResultSet res = JDBCUtil.getConnection().prepareStatement(
 					  "select * from tb_pessoa_fisica").executeQuery();
 			while (res.next()){
-				System.out.println(res.getString("nome")+" - "+res.getString("email"));
+				lista.add(new PessoaFisica(
+						res.getLong("id"),
+						res.getString("nome"),
+						res.getString("endereco"),
+						res.getString("telefone"),
+						res.getString("cpf"),
+						res.getString("email"),
+						res.getDate("data_nascimento"),
+						res.getString("sexo")
+						)
+					);
+				//System.out.println(res.getString("nome")+" - "+res.getString("email"));
 			}
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
 		} finally {
-			
+			JDBCUtil.closeConnection();
 		}
-		return null;
+		return lista;
 	}
 
 }
